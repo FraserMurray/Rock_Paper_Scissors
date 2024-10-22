cb892854b42d7a9e4edb69ef6c334c26f9d590f2
Fraser <famurray938@gmail.com>
2024-10-22 20:39:17 +0100

    Project Almost Completed, script is working as intended however still need to fix the while loop functionality tomorrow.

--- script.js
+++ script.js
@@ -1,15 +1,70 @@
-console.log("Hello World")
+// console.log("Hello World")
 
-let randomNum = Math.ceil(Math.random() * 3)
+
 
 function getComputerChoice() {
+    let randomNum = Math.ceil(Math.random() * 3);
     if (randomNum === 1) {
-        console.log("Rock");
+        return "Rock";
     } else if (randomNum === 2) {
-        console.log("Paper");
+        return "Paper";
     } else {
-        console.log("Scissors");
+        return "Scissors";
     }
 }
 
 getComputerChoice();
+
+function getHumanChoice() {
+    let decision = prompt("Let's play rock paper scissors! Please enter your choice.");
+    return decision[0].toUpperCase() + decision.substring(1).toLowerCase()
+} 
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("Human Choice: " + humanChoice);
+        console.log("Computer Choice: " + computerChoice);
+        console.log("Draw, Try Again!");
+    } else if (humanChoice === "Rock" && computerChoice === "Paper") {
+        console.log("Human Choice: " + humanChoice);
+        console.log("Computer Choice: " + computerChoice);
+        console.log("Computer: " + ++computerScore + " Human: " + humanScore);
+    } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+        console.log("Human Choice: " + humanChoice);
+        console.log("Computer Choice: " + computerChoice);
+        humanScore++;
+        console.log("Computer: " + computerScore + " Human: " + humanScore);
+    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        console.log("Human Choice: " + humanChoice);
+        console.log("Computer Choice: " + computerChoice);
+        humanScore++;
+        console.log("Computer: " + computerScore + " Human: " + humanScore);
+    } else if (humanChoice === "Paper" && computerChoice === "Scissors") {
+        console.log("Human Choice: " + humanChoice);
+        console.log("Computer Choice: " + computerChoice);
+        computerScore++;
+        console.log("Computer: " + computerScore + " Human: " + humanScore);
+    } else if (humanChoice === "Scissors" && computerChoice === "Rock") {
+        console.log("Human Choice: " + humanChoice);
+        console.log("Computer Choice: " + computerChoice);
+        computerScore++;
+        console.log("Computer: " + computerScore + " Human: " + humanScore);
+    } else {
+        console.log("Human Choice: " + humanChoice);
+        console.log("Computer Choice: " + computerChoice);
+        humanScore++;
+        console.log("Computer: " + computerScore + " Human: " + humanScore);
+    }
+}
+
+while (computerScore < 5 || humanScore < 5) {
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection,computerSelection);
+} if (humanScore === 5 || computerScore === 5) {
+    console.log("Game Over!");
+}
+
